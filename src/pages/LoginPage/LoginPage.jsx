f6479c61059a91ca4df2aac259330883dbac4b26
shaneyoung91 <shaneadam.young@gmail.com>
2023-08-11 17:04:18 -0700

    Lab Part 1 complete

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,22 @@
 import {useState} from 'react';
 
 export default function LoginPage({ user, setUser }) {
-    const [formData, setFormData] = useState("")
+    const [formData, setFormData] = useState({
+        name: ""
+    })
 
     function handleUser(evt) {
         evt.preventDefault();
-        setUser(formData)
-        setUser("Bob") // NEED TO FIX THIS
+        setUser(formData.name)
     }
+
+    function handleChange(evt) {
+        // Replace with new object and use a computed property
+        // to update the correct property
+        const newFormData = { ...formData, [evt.target.name]: evt.target.value };
+        setFormData(newFormData);
+    }
+
 
     return (
         <>
@@ -15,7 +24,7 @@ export default function LoginPage({ user, setUser }) {
             <form onSubmit={handleUser}>
                 <div>
                     <label>Username</label>
-                    <input type="text"></input>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange}></input>
                 </div>
                 <button type="submit">Log In</button>
             </form>
